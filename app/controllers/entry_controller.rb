9da5da2b6e069696b4d69de1e71331b22c4d67f3
sofia de Coulon <sofia.decoulon@kellogg.northwestern.edu>
2025-02-24 21:19:56 +0000

    HW3 - v1

---
+++ app/controllers/entry_controller.rb
@@ -0,0 +1,28 @@
+class EntryController < ApplicationController
+  def show
+    @entry = Entry.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" => @entry["place_id"]})
+    # render entrys/show view with details about entry
+  end
+  
+  def new
+    @place = Place.find_by({"id" => params["place_id"]})
+    # render entrys/new view with new entry form
+  end
+  def create
+    # start with a new entry
+    @entry = Entry.new
+    # assign user-entered form data to entry's columns
+    @entry["title"] = params["title"]
+    @entry["descrition"] = params["descrition"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    # assign relationship between entry and Place
+    @entry["place_id"] = params["place_id"]
+    # save entry row
+    @entry.save
+    # redirect user
+    redirect_to "/entry/#{@entry["place_id"]}"
+  end
+  
+end
---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,18 @@
+class PlacesController < ApplicationController
+  def index
+    @place = Place.all
+  end
+  def show
+    @place = Place.find_by({"id" => params[:id]})
+  end
+  def new
+    @place = Place.new
+  end
+  def create
+    @place["name"] = params["name"]
+     @place.save
+      
+      redirect_to "/place"
+    end
+  end
+
